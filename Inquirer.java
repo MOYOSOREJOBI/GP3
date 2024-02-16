b214c1f37dfa963625c8f7ed252384a3410ed36d
Oussama Ouadihi <144302188+Oussama-Ouadihi@users.noreply.github.com>
2024-02-15 23:46:52 -0700

    Add files via upload

---
+++ Inquirer.java
@@ -0,0 +1,31 @@
+package edu.ucalgary.oop;
+
+public class Inquirer {
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String message;
+
+    public Inquirer(String firstName, String lastName, String phoneNumber, String message) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.message = message;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
